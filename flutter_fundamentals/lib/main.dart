ae93effd311a9147d4ca82afdfd7f9e05aee5e73
sharmaharshitnone <sharmaharshitnone@gmail.com>
2026-02-21 14:12:50 +0530

    chore: remove extraneous markdown code block delimiters from main.dart

--- flutter_fundamentals/lib/main.dart
+++ flutter_fundamentals/lib/main.dart
@@ -1,4 +1,3 @@
-```dart
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -33,4 +32,4 @@ class SafeStrideApp extends StatelessWidget {
     );
   }
 }
-```
+
